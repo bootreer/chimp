e0538f0104c19d13fe250f56525580881146cf98
tuomas <ge78bej@mytum.de>
2023-03-15 23:47:42 +0100

    updated main for testing speed by doing many runs over datasets and updated readme

--- README.md
+++ README.md
@@ -1,7 +1,17 @@
 # Chimp
 [chimp compression](https://vldb.org/pvldb/vol15/p3058-liakos.pdf) in [rust](https://www.youtube.com/watch?v=dQw4w9WgXcQ). 
 
+----------------------------
+
+[chimp.rs](src/chimp.rs) and [chimpn.rs](src/chimpn.rs) implementations of compression from paper
+
 [aligned.rs](src/aligned.rs) based off of [DuckDB's implementation of a byte-aligned variation of Chimp](https://github.com/duckdb/duckdb/pull/5044)
+
+[gorilla.rs](src/gorilla.rs) compression as described in [this paper](https://www.vldb.org/pvldb/vol8/p1816-teller.pdf) and chimp paper
 
 ## my code
 ![shitsonfireyo](https://user-images.githubusercontent.com/72973431/211576509-1abf63b2-9340-4aad-908f-f6cda1ff9495.jpg)
+
+*Maybe might still do:*
+- make compression generic for f64 and f32
+- idk if this will actually ever become a crate (small rewrite probably not a bad idea in that case lol)
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use chimp_lib::{aligned, bitstream::InputBitStream, chimp, chimpn, gorilla, Decode, Encode};
-
-use std::time::Instant;
+use std::time::{Duration, Instant};
 
 #[derive(Debug)]
 pub enum ChimpType {
@@ -9,7 +8,7 @@ pub enum ChimpType {
     SIMD,
     Gorilla,
     Rayon,
-    Patas
+    Patas,
 }
 
 // simple benchmark/test/comparison with different datasets
@@ -23,7 +22,6 @@ fn main() {
         ("datasets/Stocks-Germany-sample.txt", 2),
         ("datasets/SSD_HDD_benchmarks.csv", 2),
 
-        // delete first 2-3 lines on the influxdb data
         // ("datasets/influxdb2-sample-data/air-sensor-data/air-sensor-data-annotated.csv", 4),
         // ("datasets/influxdb2-sample-data/bird-migration-data/bird-migration.csv", 6),
         // ("datasets/influxdb2-sample-data/bitcoin-price-data/bitcoin-historical-annotated.csv", 4),
@@ -54,96 +52,147 @@ fn main() {
 }
 
 pub fn test_compression(paths: &Vec<(&str, usize)>, enc_t: ChimpType) {
-    for (path, float_idx) in paths {
-        println!("[[DATASET: {}]]", path);
-        let reader = csv::Reader::from_path(path);
-        let mut values: Vec<f64> = Vec::new();
+    let mut enc_vec = Vec::new();
+    let mut dec_vec = Vec::new();
+    let mut ratio_vec = Vec::new();
 
-        for record in reader.unwrap().records() {
-            let string_record = record.unwrap();
-            let val = string_record[*float_idx].to_string();
-            let val = val.parse::<f64>().unwrap();
-            values.push(val);
-        }
+    // does n runs over each dataset, used for getting an avg dec/enc speed 
+    let n = 25;
+    for _ in 0..n {
+        for (path, float_idx) in paths {
+            // println!("[[DATASET: {}]]", path);
+            let reader = csv::Reader::from_path(path);
+            let mut values: Vec<f64> = Vec::new();
 
-        match enc_t {
-            ChimpType::Rayon => {
-                let now = Instant::now();
-                let encoded = chimp::Encoder::threaded(&values);
-                let new_now = Instant::now();
-                println!(
-                    "[encode] per 1000 values: {:?}",
-                    (new_now - now) / (values.len() / 1000) as u32
-                );
-                let size: u64 = encoded.iter().fold(0, |acc, tup| acc + tup.1);
-                println!(
-                    "average bits per val: {}",
-                    size as f64 / values.len() as f64
-                );
-                let now = Instant::now();
-                let decoded = chimp::Decoder::decode_threaded(encoded);
-                let new_now = Instant::now();
-                println!(
-                    "[decode] per 1000 values: {:?}",
-                    (new_now - now) / (decoded.len() / 1000) as u32
-                );
-                assert_eq!(decoded, values);
-            },
-            ChimpType::SIMD => {
-                let mut chimp_simd = chimp::Encoder::new();
-                let now = Instant::now();
-                unsafe {
-                    chimp_simd.simd_vec(&values);
+            for record in reader.unwrap().records() {
+                let string_record = record.unwrap();
+                let val = string_record[*float_idx].to_string();
+                let val = val.parse::<f64>().unwrap();
+                values.push(val);
+            }
+            let enc_speed: Duration;
+            let dec_speed: Duration;
+            let compr_ratio: f64;
+            match enc_t {
+                ChimpType::Rayon => {
+                    let now = Instant::now();
+                    let encoded = chimp::Encoder::threaded(&values);
+                    let new_now = Instant::now();
+                    // println!(
+                    //     "[encode] per 1000 values: {:?}",
+                    //     (new_now - now) / (values.len() / 1000) as u32
+                    // );
+                    let size: u64 = encoded.iter().fold(0, |acc, tup| acc + tup.1);
+                    // println!(
+                    //     "average bits per val: {}",
+                    //     size as f64 / values.len() as f64
+                    // );
+                    enc_speed = (new_now - now) / (values.len() / 1000) as u32;
+                    compr_ratio = size as f64 / values.len() as f64;
+
+                    let now = Instant::now();
+                    let decoded = chimp::Decoder::decode_threaded(encoded);
+                    let new_now = Instant::now();
+
+                    dec_speed = (new_now - now) / (decoded.len() / 1000) as u32;
+                    // println!(
+                    //     "[decode] per 1000 values: {:?}",
+                    //     (new_now - now) / (decoded.len() / 1000) as u32
+                    // );
+                    assert_eq!(decoded, values);
                 }
-                let new_now = Instant::now();
-                let (buffer, size) = chimp_simd.close();
-                println!(
-                    "average bits per val: {}",
-                    size as f64 / values.len() as f64
-                );
-                println!(
-                    "[encode] per 1000 values: {:?}",
-                    (new_now - now) / (values.len() / 1000) as u32
-                );
-                let mut dec = chimp::Decoder::new(InputBitStream::new(buffer));
-                let mut vec: Vec<f64> = Vec::new();
-                let now = Instant::now();
-                while let Ok(dec_val) = dec.get_next() {
-                    vec.push(f64::from_bits(dec_val));
-                }
-                let new_now = Instant::now();
-                println!(
-                    "[decode] per 1000 values: {:?}",
-                    (new_now - now) / (vec.len() / 1000) as u32
-                );
-                assert_eq!(&vec, &values);
-            }
-            ChimpType::Chimp => {
-                encode(chimp::Encoder::with_capacity(values.len()), &values, ChimpType::Chimp);
-            }
-            ChimpType::ChimpN => {
-                encode(chimpn::Encoder::with_capacity(values.len()), &values, ChimpType::ChimpN);
-            }
-            ChimpType::Gorilla => {
-                encode(gorilla::Encoder::new(), &values, ChimpType::Gorilla);
-            },
-            ChimpType::Patas => {
-                encode(aligned::Encoder::with_capacity(values.len()), &values, ChimpType::Patas);
-            }
+                ChimpType::SIMD => {
+                    let mut chimp_simd = chimp::Encoder::new();
+                    let now = Instant::now();
+                    unsafe {
+                        chimp_simd.simd_vec(&values);
+                    }
+                    let new_now = Instant::now();
+                    let (buffer, size) = chimp_simd.close();
+                    // println!(
+                    //     "average bits per val: {}",
+                    //     size as f64 / values.len() as f64
+                    // );
+                    // println!(
+                    //     "[encode] per 1000 values: {:?}",
+                    //     (new_now - now) / (values.len() / 1000) as u32
+                    // );
+                    enc_speed = (new_now - now) / (values.len() / 1000) as u32;
+                    compr_ratio = size as f64 / values.len() as f64;
 
+                    let mut dec = chimp::Decoder::new(InputBitStream::new(buffer));
+                    let mut vec: Vec<f64> = Vec::new();
+                    let now = Instant::now();
+                    while let Ok(dec_val) = dec.get_next() {
+                        vec.push(f64::from_bits(dec_val));
+                    }
+                    let new_now = Instant::now();
+
+                    dec_speed = (new_now - now) / (vec.len() / 1000) as u32;
+                    // println!(
+                    //     "[decode] per 1000 values: {:?}",
+                    //     (new_now - now) / (vec.len() / 1000) as u32
+                    // );
+                    assert_eq!(&vec, &values);
+                }
+                ChimpType::Chimp => {
+                    (compr_ratio, enc_speed, dec_speed) = encode(
+                        chimp::Encoder::with_capacity(values.len()),
+                        &values,
+                        ChimpType::Chimp,
+                    );
+                }
+                ChimpType::ChimpN => {
+                    (compr_ratio, enc_speed, dec_speed) = encode(
+                        chimpn::Encoder::with_capacity(values.len()),
+                        &values,
+                        ChimpType::ChimpN,
+                    );
+                }
+                ChimpType::Gorilla => {
+                    (compr_ratio, enc_speed, dec_speed) =
+                        encode(gorilla::Encoder::new(), &values, ChimpType::Gorilla);
+                }
+                ChimpType::Patas => {
+                    (compr_ratio, enc_speed, dec_speed) = encode(
+                        aligned::Encoder::with_capacity(values.len()),
+                        &values,
+                        ChimpType::Patas,
+                    );
+                }
+            }
+            enc_vec.push(enc_speed);
+            dec_vec.push(dec_speed);
+            ratio_vec.push(compr_ratio);
         }
     }
+    let enc_avg: Duration =
+        enc_vec.iter().fold(Duration::ZERO, |acc, dur| acc + *dur) / enc_vec.len() as u32;
+    let dec_avg: Duration =
+        dec_vec.iter().fold(Duration::ZERO, |acc, dur| acc + *dur) / dec_vec.len() as u32;
+    let compr: f64 =
+        ratio_vec.iter().fold(0f64, |acc, compr| acc + *compr) / ratio_vec.len() as f64;
+    println!(
+        "AVG Enc/1000: {:?} | AVG Dec/1000: {:?} | AVG Compr Ratio: {compr} bits/val",
+        enc_avg, dec_avg
+    );
 }
 
 // i've won but at what cost
+// returns compression ratio, time/1000 for encoding and decoding
 #[allow(unused_variables)]
-pub fn encode(mut enc: impl Encode, values: &Vec<f64>, enc_t: ChimpType) {
+pub fn encode(
+    mut enc: impl Encode,
+    values: &Vec<f64>,
+    enc_t: ChimpType,
+) -> (f64, Duration, Duration) {
     let now = Instant::now();
     for val in values {
         enc.encode(*val);
     }
     let new_now = Instant::now();
     let (bytes, size) = enc.close();
+    /*
     println!(
         "average bits per val: {}",
         size as f64 / values.len() as f64
@@ -152,18 +201,28 @@ pub fn encode(mut enc: impl Encode, values: &Vec<f64>, enc_t: ChimpType) {
         "[encode] per 1000 values: {:?}",
         (new_now - now) / (values.len() / 1000) as u32
     );
+    */
+    let enc_speed = (new_now - now) / (values.len() / 1000) as u32;
+    let compr_ratio = size as f64 / values.len() as f64;
 
     let bitstream = InputBitStream::new(bytes);
-    match enc_t {
-        ChimpType::Chimp => decode(chimp::Decoder::new(bitstream), values),
-        ChimpType::ChimpN => decode(chimpn::Decoder::new(bitstream), values),
-        ChimpType::Gorilla => decode(gorilla::Decoder::new(bitstream), values),
-        ChimpType::Patas => decode(aligned::Decoder::new(bitstream), values),
-        _ => {},
-    };
+
+    // most readable return value
+    (
+        compr_ratio,
+        enc_speed,
+        match enc_t {
+            ChimpType::Chimp => decode(chimp::Decoder::new(bitstream), values),
+            ChimpType::ChimpN => decode(chimpn::Decoder::new(bitstream), values),
+            ChimpType::Gorilla => decode(gorilla::Decoder::new(bitstream), values),
+            ChimpType::Patas => decode(aligned::Decoder::new(bitstream), values),
+            _ => Duration::ZERO,
+        },
+    )
 }
 
-pub fn decode(mut dec: impl Decode, values: &Vec<f64>) {
+#[allow(unused)]
+pub fn decode(mut dec: impl Decode, values: &Vec<f64>) -> Duration {
     let mut vec: Vec<f64> = Vec::new();
     let now = Instant::now();
 
@@ -172,9 +231,12 @@ pub fn decode(mut dec: impl Decode, values: &Vec<f64>) {
     }
 
     let new_now = Instant::now();
-    println!(
-        "[decode] per 1000 values: {:?}",
-        (new_now - now) / (vec.len() / 1000) as u32
-    );
+    // println!(
+    //     "[decode] per 1000 values: {:?}",
+    //     (new_now - now) / (vec.len() / 1000) as u32
+    // );
+
     assert_eq!(&vec, values);
+
+    (new_now - now) / (vec.len() / 1000) as u32
 }
